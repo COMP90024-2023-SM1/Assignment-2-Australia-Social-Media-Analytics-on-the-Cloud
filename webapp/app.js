e0358526680dab6b59ddae6a088df509d4c2fe44
ZIYUQ <ziyu303@foxmail.com>
2023-04-21 16:01:27 +1000

    initial template of the web app

---
+++ webapp/app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+
+const cookieParser = require('cookie-parser')
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+
+app.get('/messages', (req, res) => {
+    res.send('Hello')
+})
+
+app.get('/:universalURL', (req, res) => {
+    res.send('404 URL NOT FOUND')
+})
+const port = 8080
+app.listen(8080, () => {
+    console.log('Listening on port ' + port + '...')
+})
+
+module.exports = app
