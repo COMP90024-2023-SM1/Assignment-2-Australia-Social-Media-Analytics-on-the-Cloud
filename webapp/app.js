79948fd2500c8c64666804afa43c77b5bbc726c8
ZIYUQ <ziyu303@foxmail.com>
2023-04-26 22:20:51 +1000

    Revert "Webapp (#1)"
    This reverts commit 7e9dce502bcf4b8b11148838762f9c44723ba793.

--- webapp/app.js
+++ webapp/app.js
@@ -1,11 +1,10 @@
 const express = require('express')
 const app = express()
-const cors = require('cors')
+
 const cookieParser = require('cookie-parser')
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
-app.use(cors)
 
 app.get('/messages', (req, res) => {
     res.send('Hello')
