8062895a58132342cbfa9be27c714bcd35cf9a46
Zhi Hern Tom <23621760+tzhern@users.noreply.github.com>
2023-04-28 18:13:36 +1000

    Fix cluster setup issues

--- ansible/mrc/roles/setup/couch/tasks/setup_couch_cluster.sh
+++ ansible/mrc/roles/setup/couch/tasks/setup_couch_cluster.sh
@@ -1,98 +1,77 @@
 #!/bin/bash
 
-PASSWORD="admin"
 NODES=("172.26.128.215" "172.26.128.179" "172.26.132.88")
-MANAGER_NODE=${NODES[0]}
+PORT=5984
+MASTER_NODE=${NODES[0]}
+CURRENT_NODE=$(ip addr show eth0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
+OTHER_NODES=`echo ${NODES[@]} | sed s/${MASTER_NODE}//`
+USERNAME="admin"
+PASSWORD="admin"
+COOKIE="a192aeb9904e6590849337933b000c99"
+VERSION="3.2.1"
 
-SSH_KEY_PATH="default.key"
+if sudo docker image ls | awk '{print $1":"$2}' | grep -q "couchdb:${VERSION}"; then
+  echo "CouchDB3 with version ${VERSION} is already installed."
+else
+  echo "Pulling CouchDB3 with version ${VERSION}..."
+  sudo docker pull couchdb:${VERSION}
+fi
 
-init_swarm() {
-  echo "Initializing swarm on manager node..."
-  ssh -i "$SSH_KEY_PATH" "$MANAGER_NODE" "sudo docker swarm init"
-}
-
-join_swarm() {
-  join_token=$(ssh -i "$SSH_KEY_PATH" "$MANAGER_NODE" "sudo docker swarm join-token worker -q")
-  echo "Join token: $join_token"
-  for node in "${NODES[@]:1}"; do
-    echo "Joining swarm on $node"
-    ssh -i "$SSH_KEY_PATH" "$node" "sudo docker swarm join --token $join_token $MANAGER_NODE:2377"
+if [[ "${CURRENT_NODE}" == "${MASTER_NODE}" ]]; then
+  CONTAINER_NAME="couch_master_${CURRENT_NODE}"
+else
+  for (( i=1; i<${#NODES[@]}; i++ )); do
+    if [[ "${NODES[$i]}" == "${CURRENT_NODE}" ]]; then
+      CONTAINER_NAME="couch_slave${i}_${CURRENT_NODE}"
+      break
+    fi
   done
-}
+fi
 
-create_couchdb_service() {
-  echo "Creating CouchDB service on swarm..."
-  ssh -i "$SSH_KEY_PATH" "$MANAGER_NODE" "sudo docker network create --driver overlay --attachable couchdb_network"
-  ssh -i "$SSH_KEY_PATH" "$MANAGER_NODE" "sudo docker service create \
-    --name couchdb \
-    --network couchdb_network \
-    --replicas ${#NODES[@]} \
-    -p 5984:5984 \
-    --env COUCHDB_USER=admin \
-    --env COUCHDB_PASSWORD=${PASSWORD} \
-    --env NODENAME={{.Node.Hostname}} \
-    --env ERL_FLAGS='-setcookie couchdb_cluster -name couchdb@{{.Node.Hostname}}' \
-    couchdb:latest"
-}
+sudo docker create \
+  --name "${CONTAINER_NAME}" \
+  --network host \
+  --env COUCHDB_USER=${USERNAME} \
+  --env COUCHDB_PASSWORD=${PASSWORD} \
+  --env COUCHDB_SECRET=${COOKIE} \
+  --env ERL_FLAGS="-setcookie \"${COOKIE}\" -name \"couchdb@${CURRENT_NODE}\" -kernel inet_dist_listen_min 9100 -kernel inet_dist_listen_max 9100 -kernel inet_dist_use_interface {0,0,0,0}" \
+  couchdb:${VERSION}
 
-setup_cluster() {
-  echo "Setting up CouchDB cluster..."
-  MAIN_NODE=${NODES[0]}
-  MAIN_NODE_URL="http://admin:${PASSWORD}@${MAIN_NODE}:5984"
+sudo docker start "${CONTAINER_NAME}"
 
-  sleep 10
+sleep 10
 
-  # Enable cluster mode
-  echo "Enabling cluster mode..."
-  curl -X POST "${MAIN_NODE_URL}/_cluster_setup" \
-    -H "Content-Type: application/json" \
-    -d "{
-      \"action\": \"enable_cluster\",
-      \"bind_address\": \"0.0.0.0\",
-      \"username\": \"admin\",
-      \"password\": \"${PASSWORD}\",
-      \"port\": 5984,
-      \"node_count\": \"${#NODES[@]}\"
-    }"
+# Check if the current node is the master node
+if [ "${CURRENT_NODE}" == "${MASTER_NODE}" ]; then
+  echo "Setting up CouchDB cluster on master node ${MASTER_NODE}..."
+  sleep 5
+  for node in ${OTHER_NODES}; do
+    echo "Enable cluster mode on node ${node}..."
+    curl -XPOST "http://${USERNAME}:${PASSWORD}@${MASTER_NODE}:${PORT}/_cluster_setup" \
+      --header "Content-Type: application/json" \
+      --data "{\"action\": \"enable_cluster\", \"bind_address\":\"0.0.0.0\",\
+               \"username\": \"${USERNAME}\", \"password\":\"${PASSWORD}\", \"port\": \"${PORT}\",\
+               \"remote_node\": \"${node}\", \"node_count\": \"$(echo ${NODES[@]} | wc -w)\",\
+               \"remote_current_user\":\"${USERNAME}\", \"remote_current_password\":\"${PASSWORD}\"}"
+  done
 
-  # Add other nodes to the cluster
-    for node in "${NODES[@]:1}"; do
-    echo "Adding node $node to cluster..."
-    curl -X POST "${MAIN_NODE_URL}/_cluster_setup" \
-      -H "Content-Type: application/json" \
-      -d "{
-        \"action\": \"add_node\",
-        \"host\": \"${node}\",
-        \"port\": 5984,
-        \"username\": \"admin\",
-        \"password\": \"${PASSWORD}\"
-      }"
-    done
+  echo "Adding nodes to cluster..."
+  for node in ${OTHER_NODES}; do
+    echo "Adding node ${node} to cluster..."
+    curl -XPOST "http://${USERNAME}:${PASSWORD}@${MASTER_NODE}:${PORT}/_cluster_setup" \
+      --header "Content-Type: application/json" \
+      --data "{\"action\": \"add_node\", \"host\":\"${node}\",\
+               \"port\": \"${PORT}\", \"username\": \"${USERNAME}\", \"password\":\"${PASSWORD}\"}" \
+      --max-time 300
+  done
 
-  # Finish cluster setup
-    echo "Finishing cluster setup..."
-  curl -X POST "${MAIN_NODE_URL}/_cluster_setup" \
-    -H "Content-Type: application/json" \
-    -d '{"action": "finish_cluster"}'
-}
+  echo "Finishing cluster setup..."
+  curl -XPOST "http://${USERNAME}:${PASSWORD}@${MASTER_NODE}:${PORT}/_cluster_setup" \
+    --header "Content-Type: application/json" --data "{\"action\": \"finish_cluster\"}"
 
-check_cluster_configuration() {
-  echo "Checking cluster configuration..."
+else
+    echo "This node (${CURRENT_NODE}) is not the master node. Skipping cluster setup."
+fi
 
-  MAIN_NODE=${NODES[0]}
-  MAIN_NODE_URL="http://admin:${PASSWORD}@${MAIN_NODE}:5984"
-
-  # Query the _membership endpoint to get information about the nodes in the cluster
-  curl -s "${MAIN_NODE_URL}/_membership"
-}
-
-
-main() {
-  init_swarm
-  join_swarm
-  create_couchdb_service
-  setup_cluster
-  check_cluster_configuration
-}
-
-main
+echo "Checking cluster status..."
+curl -X GET "http://${USERNAME}:${PASSWORD}@${CURRENT_NODE}:${PORT}/_membership"
