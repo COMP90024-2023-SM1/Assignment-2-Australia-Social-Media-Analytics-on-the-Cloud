4619f23e8de32fabdfdc286e544ade07426e8c5a
Haonan Zhong <616012788@qq.com>
2023-05-02 22:06:46 +1000

    Updated couchDB process method to batch processing
    - Speed up the process of reading tweets into couchDB database

--- src/tweet_couchdb_access.py
+++ src/tweet_couchdb_access.py
@@ -10,17 +10,15 @@ PASSWORD = 'admin'
 class DataStore():
 
     def __init__(self, db_name, url=f'http://{ADMIN}:{PASSWORD}@{MASTER_NODE}:5984/'):
-        self.server = couchdb.Server(url)
-        if db_name not in self.server:
+        try:
+            self.server = couchdb.Server(url=url)
             self.db = self.server.create(db_name)
             # self._create_views()
-        else:
+        except couchdb.http.PreconditionFailed:
             self.db = self.server[db_name]
 
-    def save_record(self, one_record):
-        one_record = json.dumps(one_record)
-        one_record = json.loads(one_record)
-        self.db.save(one_record)
+    def save_record(self, record_batch):
+        self.db.update(record_batch)
 
     # def _create_views(self):
     #     count_map = 'function(doc) { emit(doc.id, 1); }'
@@ -37,6 +35,7 @@ class DataStore():
 
     def tweet_processor(self, tweet_path, block_start, block_end):
 
+        docs_to_insert = []
         with open(tweet_path, 'rb') as file:
             file.seek(block_start)
             while file.tell() != block_end:
@@ -48,7 +47,18 @@ class DataStore():
                     tweet = json.loads(line)
                     if tweet['doc']['data']['geo'] != {}:
                         tweet = extract_tweet_info(tweet)
-                        self.save_record(tweet)
+                        tweet = json.dumps(tweet)
+                        tweet = json.loads(tweet)
+                        docs_to_insert.append(tweet)
 
                 except (KeyError, ValueError) as e:
                     pass
+
+                # Inserting tweets by batch to speed up the process
+                if len(docs_to_insert) == 1000:
+                    self.save_record(docs_to_insert)
+                    docs_to_insert = []
+
+            # Insert the remaining tweets
+            if len(docs_to_insert) != 0:
+                self.save_record(docs_to_insert)
