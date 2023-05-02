f5de7445b406b1315f74af6253f1bade82b2ce06
Haonan Zhong <616012788@qq.com>
2023-05-02 16:47:52 +1000

    Fixed twitter data time parsing error

--- src/tweet_couchdb_access.py
+++ src/tweet_couchdb_access.py
@@ -3,17 +3,17 @@ import json
 
 from tweet_preprocess_utils import *
 
-MASTER_NODE = 'localhost'
+MASTER_NODE = '172.26.134.229'
 ADMIN = 'admin'
 PASSWORD = 'admin'
 
 class DataStore():
 
-    def __init__(self, db_name, url=f'http://{ADMIN}:{PASSWORD}@127.0.0.1:5984/'):
+    def __init__(self, db_name, url=f'http://{ADMIN}:{PASSWORD}@{MASTER_NODE}:5984/'):
         self.server = couchdb.Server(url)
         if db_name not in self.server:
             self.db = self.server.create(db_name)
-            self._create_views()
+            # self._create_views()
         else:
             self.db = self.server[db_name]
 
@@ -22,18 +22,18 @@ class DataStore():
         one_record = json.loads(one_record)
         self.db.save(one_record)
 
-    def _create_views(self):
-        count_map = 'function(doc) { emit(doc.id, 1); }'
-        count_reduce = 'function(keys, values) { return sum(values); }'
-        view = couchdb.design.ViewDefinition('twitter', 
-                                             'count_tweets', 
-                                             count_map, 
-                                             reduce_fun=count_reduce)
-        view.sync(self.db)
+    # def _create_views(self):
+    #     count_map = 'function(doc) { emit(doc.id, 1); }'
+    #     count_reduce = 'function(keys, values) { return sum(values); }'
+    #     view = couchdb.design.ViewDefinition('twitter', 
+    #                                          'count_tweets', 
+    #                                          count_map, 
+    #                                          reduce_fun=count_reduce)
+    #     view.sync(self.db)
 
-    def count_tweets(self):
-        for doc in self.db.view('twitter/count_tweets'):
-            return doc.value
+    # def count_tweets(self):
+    #     for doc in self.db.view('twitter/count_tweets'):
+    #         return doc.value
 
     def tweet_processor(self, tweet_path, block_start, block_end):
 
