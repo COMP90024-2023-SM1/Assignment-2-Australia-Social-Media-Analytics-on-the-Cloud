489bc5e2d6f416b48d7275ae831adde393631028
Zhi Hern Tom <23621760+tzhern@users.noreply.github.com>
2023-05-13 15:12:58 +1000

    Change end_date to 2023 June 30

--- src/mastodon_legacy_social/main.py
+++ src/mastodon_legacy_social/main.py
@@ -6,13 +6,11 @@ def main():
     store_mastodon = MastodonData(db_name = 'legacy_mastodon_social_data')
 
     start_date = datetime(2022, 2, 1)
-    end_date = datetime(2023, 5, 30)
+    end_date = datetime(2023, 6, 30)
 
     store_mastodon.mastodon_processor(start_date, end_date)
 
 if __name__ == "__main__":
     main()
-
-
 
 
