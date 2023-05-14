37d10ee2772a7115a6279e20c912232c483b873b
Haonan Zhong <616012788@qq.com>
2023-05-14 14:57:08 +1000

    Updated helper.R

--- myShinyApp/helper.R
+++ myShinyApp/helper.R
@@ -8,7 +8,7 @@ library(tidyr)
 total_tweet <- GET("http://admin:admin@172.26.128.113:5984/twitter_data/_design/customDoc/_view/count-total?reduce=true&group=true&update=false")
 total_tweet <- content(total_tweet, "parsed")
 
-generalTweet_info <- GET("http://admin:admin@172.26.128.113:5984/twitter_data/_design/customDoc/_view/count-by-gcc?reduce=true&group=true")
+generalTweet_info <- GET("http://admin:admin@172.26.128.113:5984/twitter_data/_design/customDoc/_view/count-by-gcc?reduce=true&group=true&update=false")
 generalTweet_info <- as.data.frame(fromJSON(content(generalTweet_info, "text", encoding = "UTF-8"))$rows)
 generalTweet_info <- generalTweet_info[generalTweet_info$key != "9OTER", ]
 location_mapping <- c("1GSYD" = "Sydney", "2GMEL" = "Melbourne", "3GBRI" = "Brisbane", 
