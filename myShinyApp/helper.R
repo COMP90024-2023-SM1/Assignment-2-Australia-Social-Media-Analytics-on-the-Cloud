17868b65c22c14f355f0155fe5f326b52519ceae
haonan99 <616012788@qq.com>
2023-05-14 10:09:06 +1000

    Added prototype shiny R code

---
+++ myShinyApp/helper.R
@@ -0,0 +1,135 @@
+library(httr)
+library(jsonlite)
+library(lubridate)
+library(dplyr)
+library(tidyr)
+
+# Helper functions
+total_tweet <- GET("http://admin:admin@172.26.128.113:5984/twitter_data/_design/customDoc/_view/count-total?reduce=true&group=true&update=false")
+total_tweet <- content(total_tweet, "parsed")
+
+generalTweet_info <- GET("http://admin:admin@172.26.128.113:5984/twitter_data/_design/customDoc/_view/average-sentiment?reduce=true&group=true&update=false")
+generalTweet_info <- as.data.frame(fromJSON(content(generalTweet_info, "text", encoding = "UTF-8"))$rows)
+generalTweet_info <- generalTweet_info[generalTweet_info$key != "9OTER", ]
+location_mapping <- c("1GSYD" = "Sydney", "2GMEL" = "Melbourne", "3GBRI" = "Brisbane", 
+                      "4GADE" = "Adelaide", "5GPER" = "Perth", "6GHOB" = "Hobart", 
+                      "7GDAR" = "Darwin", "8ACTE" = "Canberra", "9OTER" = "Other", 
+                      "1RNSW" = "Rural NSW", "2RVIC" = "Rural VIC", "3RQLD" = "Rural QLD",
+                      "4RSAU" = "Rural SA", "5RWAU" = "Rural WA", "6RTAS" = "Rural TAS",
+                      "7RNTE" = "Rural NT")
+generalTweet_info$key <- location_mapping[generalTweet_info$key]
+
+capital_cities <- data.frame(
+  key = c("Canberra", "Sydney", "Melbourne", "Brisbane", "Perth", "Adelaide", "Darwin", "Hobart",
+           "Rural VIC", "Rural SA", "Rural NSW", "Rural QLD", "Rural NT", "Rural WA", "Rural TAS"),
+  lat = c(-35.2809, -33.8688, -37.8136, -27.4698, -31.9505, -34.9285, -12.4634, -42.8821, 
+          -36.546835, -30.461157, -31.000383, -22.395562, -20.932729, -25.382024, -41.276145),
+  lon = c(149.1300, 151.2093, 144.9631, 153.0251, 115.8605, 138.6007, 130.8456, 147.3272, 
+          143.049673, 134.602050, 145.057544, 144.241518, 133.261762, 121.899317, 145.029348)
+)
+
+generalTweet_info <- merge(generalTweet_info, capital_cities, by = "key")
+
+# Theme for dashboard
+customTheme <- shinyDashboardThemeDIY(
+  ### general
+  appFontFamily = "Optima"
+  ,appFontColor = "#2D2D2D"
+  ,primaryFontColor = "#000000"
+  ,infoFontColor = "#000000"
+  ,successFontColor = "#0F0F0F"
+  ,warningFontColor = "#D41A1A"
+  ,dangerFontColor = "#D41A1A"
+  ,bodyBackColor = "#FFFFFF"
+  
+  ### header
+  ,logoBackColor = "#FFFFFF"
+  
+  ,headerButtonBackColor = "#FFFFFF"
+  ,headerButtonIconColor = "#000000"
+  ,headerButtonBackColorHover = "#CAE0E6"
+  ,headerButtonIconColorHover = "#000000"
+  
+  ,headerBackColor = "#FFFFFF"
+  ,headerBoxShadowColor = ""
+  ,headerBoxShadowSize = "0px 0px 0px"
+  
+  ### sidebar
+  ,sidebarBackColor = "#F0F0F0"
+  ,sidebarPadding = "3"
+  
+  ,sidebarMenuBackColor = "transparent"
+  ,sidebarMenuPadding = "2"
+  ,sidebarMenuBorderRadius = 0
+  
+  ,sidebarShadowRadius = ""
+  ,sidebarShadowColor = "0px 0px 0px"
+  
+  ,sidebarUserTextColor = "#737373"
+  
+  ,sidebarSearchBackColor = "#FFFFFF"
+  ,sidebarSearchIconColor = "#000000"
+  ,sidebarSearchBorderColor = "#DCDCDC"
+  
+  ,sidebarTabTextColor = "#737373"
+  ,sidebarTabTextSize = "15"
+  ,sidebarTabBorderStyle = "none"
+  ,sidebarTabBorderColor = "none"
+  ,sidebarTabBorderWidth = "0"
+  
+  ,sidebarTabBackColorSelected = "#D1D1D1"
+  ,sidebarTabTextColorSelected = "#000000"
+  ,sidebarTabRadiusSelected = "0px"
+  
+  ,sidebarTabBackColorHover = "#F5F5F5"
+  ,sidebarTabTextColorHover = "#000000"
+  ,sidebarTabBorderStyleHover = "none solid none none"
+  ,sidebarTabBorderColorHover = "#C8C8C8"
+  ,sidebarTabBorderWidthHover = "4"
+  ,sidebarTabRadiusHover = "0px"
+  
+  ### boxes
+  ,boxBackColor = "#FFFFFF"
+  ,boxBorderRadius = "5"
+  ,boxShadowSize = "none"
+  ,boxShadowColor = ""
+  ,boxTitleSize = "20"
+  ,boxDefaultColor = "#E1E1E1"
+  ,boxPrimaryColor = "#5F9BD5"
+  ,boxInfoColor = "#B4B4B4"
+  ,boxSuccessColor = "#70AD47"
+  ,boxWarningColor = "#ED7D31"
+  ,boxDangerColor = "#E84C22"
+  
+  ,tabBoxTabColor = "#F8F8F8"
+  ,tabBoxTabTextSize = "15"
+  ,tabBoxTabTextColor = "#646464"
+  ,tabBoxTabTextColorSelected = "#2D2D2D"
+  ,tabBoxBackColor = "#F8F8F8"
+  ,tabBoxHighlightColor = "#C8C8C8"
+  ,tabBoxBorderRadius = "5"
+  
+  ### inputs
+  ,buttonBackColor = "#E2D2FA"
+  ,buttonTextColor = "#2D2D2D"
+  ,buttonBorderColor = "#FFFFFF"
+  ,buttonBorderRadius = "9"
+  
+  ,buttonBackColorHover = "#BEBEBE"
+  ,buttonTextColorHover = "#000000"
+  ,buttonBorderColorHover = "#969696"
+  
+  ,textboxBackColor = "#FFFFFF"
+  ,textboxBorderColor = "#6C6C6C"
+  ,textboxBorderRadius = "9"
+  ,textboxBackColorSelect = "#F5F5F5"
+  ,textboxBorderColorSelect = "#6C6C6C"
+  
+  ### tables
+  ,tableBackColor = "#F8F8F8"
+  ,tableBorderColor = "#EEEEEE"
+  ,tableBorderTopSize = "5"
+  ,tableBorderRowSize = "4"
+)
+
+ruralcity_choiceVec <- c('Greater Capital City', 'Rural')
---
+++ myShinyApp/server.R
@@ -0,0 +1,84 @@
+# Server
+
+# Importing libraries
+library(shiny)
+library(httr)
+library(stringr)
+library(shinythemes)
+library(fontawesome)
+library(shinydashboard)
+library(igraph)
+library(lubridate)
+library(highcharter)
+library(dplyr)
+library(tidyr)
+library(dashboardthemes)
+source('helper.R')
+
+# Set a global theme highcharter plot
+options(highcharter.theme = hc_theme_hcrt())
+
+server <- shinyServer(function(input, output) {
+  
+  auto_refresh <- reactiveTimer(100000)
+  
+  get_api_data <- reactive({
+    auto_refresh()
+    
+    tweet_month <- GET('http://admin:admin@172.26.128.113:5984/twitter_data/_design/customDoc/_view/count-by-month?reduce=true&group=true&update=false')
+    tweet_month <- fromJSON(content(tweet_month, "text", encoding = "UTF-8"))$rows
+    tweet_month <- tweet_month %>%
+      mutate(key = sapply(key, function(x) paste(unlist(x), collapse = ", ")))
+    tweet_month <- tweet_month %>%
+      separate(key, into = c("year", "month"), sep = ", ") %>%
+      mutate(
+        date = paste(year, month, "01", sep = "-"),
+        date = ymd(date),
+        month_label = month(date, label = TRUE),
+        YearMonth = paste(month_label, year, sep = " ")
+      ) %>%
+      select(-date, -month_label)
+    
+    return(tweet_month)
+  })
+  
+  # Value box for incident count
+  output$total_tweet <- renderValueBox({
+    print(input$dateRange)
+    valueBox(
+      value = total_tweet$rows[[1]]$value, subtitle = "Total Tweets",
+      icon = fa_i("twitter"), color = "light-blue"
+    )
+  })
+
+  
+  output$aus_map <- renderHighchart({
+    if (length(input$map_state) == 2) {
+      data <- generalTweet_info
+    } else if (input$map_state == 'Greater Capital City') {
+      data <- subset(generalTweet_info, !grepl("^Rural", key))
+    } else {
+      data <- subset(generalTweet_info, grepl("^Rural", key))
+    }
+      
+    
+    hcmap("countries/au/au-all", borderColor = "#808080", borderWidth = 0.1, showInLegend = FALSE) %>%
+      hc_exporting(enabled = TRUE) %>%
+      hc_chart(backgroundColor = "#D8F9FF") %>%
+      hc_add_series(name = "Cities/Areas",
+        type = "mappoint", data = data, hcaes(x = lon, y = lat, name = key), color = "red",
+        marker = list(radius = 5)) %>%
+      hc_title(text = "General Tweet Statistics of Australia in 2022 <small>(Hover for more detail)</small>", 
+               useHTML = T)
+  })
+  
+  output$tweet_timeline <- renderHighchart({
+    tweet_month <- get_api_data()
+    hchart(tweet_month, "spline", hcaes(x = YearMonth, y = value),
+           tooltip = list(pointFormat = "Number of Tweets Made: <b>{point.value}</b>")) %>%
+      hc_title(text = "Number of Tweets Made in 2022") %>%
+      hc_yAxis(title = list(text = "Number of Tweets")) %>%
+      hc_xAxis(title = list(text = "Month"))
+  })
+
+})
