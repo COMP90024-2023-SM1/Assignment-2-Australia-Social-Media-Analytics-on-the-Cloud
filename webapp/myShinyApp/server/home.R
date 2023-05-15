512735638c5ac377ae5957cbf857b6bc79b896d0
Haonan Zhong <616012788@qq.com>
2023-05-15 11:25:21 +1000

    Added value box for mastodon total toots

--- webapp/myShinyApp/server/home.R
+++ webapp/myShinyApp/server/home.R
@@ -17,7 +17,7 @@ library(rgdal)
 source('helper.R')
   
 serverHome = function(input, output){
-  auto_refresh <- reactiveTimer(100000)
+  auto_refresh <- reactiveTimer(20000)
   get_tweet_month <- reactive({
     auto_refresh()
     tweet_month <- GET('http://admin:admin@172.26.128.113:5984/twitter_data/_design/customDoc/_view/count-by-month?reduce=true&group=true&update=false')
@@ -37,11 +37,33 @@ serverHome = function(input, output){
     return(tweet_month)
   })
   
+  get_mastodon_count <- reactive({
+    auto_refresh()
+    legacy_social_count <- GET('http://172.26.128.113:5984/legacy_mastodon_social_data/_design/customDoc/_view/count-total?reduce=true&group=true')
+    legacy_social_count <- fromJSON(httr::content(legacy_social_count, "text", encoding = "UTF-8"))$rows$value
+    legacy_world_count <- GET('http://172.26.128.113:5984/legacy_mastodon_world_data/_design/customDoc/_view/count-total?reduce=true&group=true&update=false')
+    legacy_world_count <- fromJSON(httr::content(legacy_world_count, "text", encoding = "UTF-8"))$rows$value
+    stream_social_count <- GET('http://172.26.128.113:5984/streaming_mastodon_social_data/_design/customDoc/_view/count-total?reduce=true&group=true&update=false')
+    stream_social_count <- fromJSON(httr::content(stream_social_count, "text", encoding = "UTF-8"))$rows$value
+    stream_world_count <- GET('http://172.26.128.113:5984/streaming_mastodon_world_data/_design/customDoc/_view/count-total?reduce=true&group=true&update=false')
+    stream_world_count <- fromJSON(httr::content(stream_world_count, "text", encoding = "UTF-8"))$rows$value
+    total_count <- legacy_social_count + legacy_world_count + stream_social_count + stream_world_count
+    
+    return(total_count)
+  })
+  
   # Value box for incident count
   output$total_tweet <- renderValueBox({
     valueBox(
       value = total_tweet$rows[[1]]$value, subtitle = "Total Tweets",
       icon = fa_i("twitter"), color = "light-blue"
+    )
+  })
+  
+  output$total_mastodon <- renderValueBox({
+    valueBox(
+      value = get_mastodon_count(), subtitle = "Total Toots",
+      icon = fa_i("mastodon"), color = "blue"
     )
   })
   
