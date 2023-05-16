f23c480a80efecb61ad422f97dfe61fe01b53076
Zhi Hern Tom <23621760+tzhern@users.noreply.github.com>
2023-05-16 18:46:18 +1000

    Add dynamic ip_address support

--- src/mastodon_legacy_world/mastodon_couchdb_access_world.py
+++ src/mastodon_legacy_world/mastodon_couchdb_access_world.py
@@ -4,14 +4,29 @@ import requests
 from datetime import datetime, timedelta, timezone
 import time
 from mastodon_preprocess_utils import *
+import socket
+import fcntl
+import struct
 
-MASTER_NODE = '172.26.134.229'
+def get_ip_address(interface):
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        ip_address = socket.inet_ntoa(fcntl.ioctl(
+            sock.fileno(),
+            0x8915,  # SIOCGIFADDR
+            struct.pack('256s', interface[:15].encode())
+        )[20:24])
+        return ip_address
+    except IOError:
+        assert False, "Unable to retrieve IP Address for " + interface
+
+NODE = get_ip_address('eth0') # Please manually set to specific Couchdb address when running on local machine
 ADMIN = 'admin'
 PASSWORD = 'admin'
 
 class MastodonData():
 
-    def __init__(self, db_name, url=f'http://{ADMIN}:{PASSWORD}@{MASTER_NODE}:5984/'):
+    def __init__(self, db_name, url=f'http://{ADMIN}:{PASSWORD}@{NODE}:5984/'):
         try:
             self.server = couchdb.Server(url=url)
             self.db = self.server.create(db_name)
